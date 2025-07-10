9b44bc199ed27ce424f75b30231838f7197817dc
mutiiaradzkr <mutiaraajikro@gmail.com>
2025-07-10 08:48:04 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Fungsi scroll ke atas halaman
+function scrollToTop() {
+  document.getElementById("home").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToMenu() {
+  document.getElementById("product").scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToKontak() {
+  document.getElementById("kontak").scrollIntoView({ behavior: 'smooth' });
+}
